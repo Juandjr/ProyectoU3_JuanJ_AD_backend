04eb5f787ee71b014c73e5cf22574d5d7efbd74e
Juandjr <jdjimenez11@espe.edu.ec>
2026-07-31 00:19:46 -0500

    Implementacion de blob base para las imagenes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profile.controller');
 const { jwtMiddleware } = require('../middleware/auth.middleware');
+const profileAvatarRoutes = require('./profile-avatar.routes');
 
 router.get('/profile', jwtMiddleware, profileController.getProfile);
+router.use(profileAvatarRoutes);
 
 module.exports = router;
